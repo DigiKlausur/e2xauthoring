42f2093d1f436c9aead196dfd7b659cae36b00da
Tim Daniel Metzler <tim.metzler@h-brs.de>
2022-08-31 14:50:28 +0200

    Add routing

---
+++ packages/app/src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import AppWithNav from "./AppWithNav";
+
+import Assignments from "./pages/Assignments";
+import TaskPools from "./pages/TaskPools";
+import TaskPool from "./pages/TaskPool";
+import Task from "./pages/Task";
+import Test from "./pages/Test";
+import Templates from "./pages/Templates";
+import Template from "./pages/Template";
+import FileDiff from "./pages/FileDiff";
+import AppWithoutNav from "./AppWithoutNav";
+import Worksheets from "./pages/Worksheets";
+import NewExercise from "./pages/NewExercise";
+
+import { pathJoin } from "@e2xauthoring/api";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path={pathJoin([window.base_url, "/e2x/authoring/app/nonav"])}
+        element={<AppWithoutNav />}
+      >
+        <Route path="diff/:pool/:task" element={<FileDiff />} />
+      </Route>
+      <Route
+        path={pathJoin([window.base_url, "/e2x/authoring/app"])}
+        element={<AppWithNav />}
+      >
+        <Route path="assignments" element={<Assignments />} />
+        <Route path="assignments/:assignment" element={<Worksheets />} />
+        <Route
+          path="assignments/:assignment/new/:name"
+          element={<NewExercise />}
+        />
+        <Route path="templates" element={<Templates />} />
+        <Route path="templates/:template" element={<Template />} />
+        <Route path="pools" element={<TaskPools />} />
+        <Route path="pools/:pool" element={<TaskPool />} />
+        <Route path="pools/:pool/:task" element={<Task />} />
+        <Route path="diff/:pool/:task" element={<FileDiff />} />
+        <Route path="test" element={<Test />} />
+        <Route index element={<Navigate to="assignments" replace />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
