7d9beb1eb61bb87046deb26ce08dda9f35e3d428
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-07-26 14:38:27 +0200

    Add models from e2xgrader

--- e2xauthoring/models/__init__.py
+++ e2xauthoring/models/__init__.py
@@ -1,4 +1,15 @@
+from .assignmentmodel import AssignmentModel
+from .exercisemodel import ExerciseModel
+from .presetmodel import PresetModel
 from .taskmodel import TaskModel
 from .taskpoolmodel import TaskPoolModel
+from .templatemodel import TemplateModel
 
-__all__ = ["TaskModel", "TaskPoolModel"]
+__all__ = [
+    "AssignmentModel",
+    "ExerciseModel",
+    "PresetModel",
+    "TaskModel",
+    "TaskPoolModel",
+    "TemplateModel",
+]
