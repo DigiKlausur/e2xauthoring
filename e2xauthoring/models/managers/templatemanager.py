b6f06b833725f00efd0a2b49a23a2ec2195d1d57
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-07-28 18:00:38 +0200

    Remove status message from TemplateManager

--- e2xauthoring/models/managers/templatemanager.py
+++ e2xauthoring/models/managers/templatemanager.py
@@ -6,7 +6,7 @@ from e2xcore.utils.nbgrader_cells import new_read_only_cell
 from nbformat.v4 import new_notebook
 from traitlets import Unicode
 
-from ..dataclasses import ErrorMessage, SuccessMessage, Template
+from ..dataclasses import Template
 from .manager import BaseManager
 
 
@@ -15,22 +15,17 @@ class TemplateManager(BaseManager):
         "templates", help="The relative directory where the templates are stored"
     )
 
-    def get(self, name: str) -> SuccessMessage | ErrorMessage:
+    def get(self, name: str):
         path = os.path.join(self.base_path, name)
-        if not os.path.exists(path):
-            return ErrorMessage(
-                error=f"A template with the name {name} does not exists!"
-            )
-        return SuccessMessage(data=Template(name=name))
+        assert os.path.exists(path), f"A template with the name {name} does not exists!"
+        return Template(name=name)
 
-    def create(self, name: str) -> SuccessMessage | ErrorMessage:
-        if not self.is_valid_name(name):
-            return ErrorMessage(error="The name is invalid!")
+    def create(self, name: str):
+        assert self.is_valid_name(name), "The name is invalid!"
         path = os.path.join(self.base_path, name)
-        if os.path.exists(path):
-            return ErrorMessage(
-                error=f"A template with the name {name} already exists!"
-            )
+        assert not os.path.exists(
+            path
+        ), f"A template with the name {name} already exists!"
         self.log.info(f"Creating new template with name {name}")
         os.makedirs(os.path.join(self.base_path, name, "img"), exist_ok=True)
         os.makedirs(os.path.join(self.base_path, name, "data"), exist_ok=True)
@@ -45,32 +40,23 @@ class TemplateManager(BaseManager):
         cell.metadata["nbassignment"] = dict(type="header")
         nb.cells.append(cell)
         nbformat.write(nb, os.path.join(self.base_path, name, f"{name}.ipynb"))
-        return SuccessMessage()
 
-    def remove(self, name: str) -> SuccessMessage | ErrorMessage:
+    def remove(self, name: str):
         path = os.path.join(self.base_path, name)
-        if not os.path.exists(path):
-            return ErrorMessage(error="The template does not exist")
+        assert os.path.exists(path), f"The template {name} does not exist."
         shutil.rmtree(path)
-        return SuccessMessage()
 
-    def list(self) -> SuccessMessage | ErrorMessage:
-        if not os.path.exists(self.base_path):
-            return ErrorMessage(error="Template directory not found.")
-        return SuccessMessage(
-            data=[
-                Template(name=template_dir)
-                for template_dir in self.listdir(self.base_path)
-            ]
-        )
+    def list(self):
+        assert os.path.exists(self.base_path), "Template directory not found."
+        templates = [
+            Template(name=template_dir) for template_dir in self.listdir(self.base_path)
+        ]
+        return templates
 
-    def copy(self, old_name: str, new_name: str) -> SuccessMessage | ErrorMessage:
-        status = super().copy(old_name, new_name)
-        if not status.success:
-            return status
+    def copy(self, old_name: str, new_name: str):
+        super().copy(old_name, new_name)
         dst_path = os.path.join(self.base_path, new_name)
         shutil.move(
             os.path.join(dst_path, f"{old_name}.ipynb"),
             os.path.join(dst_path, f"{new_name}.ipynb"),
         )
-        return SuccessMessage()
