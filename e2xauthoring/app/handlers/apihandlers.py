a53290061743f8850f31c285206b759d319d64c9
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-07-28 18:31:13 +0200

    Use new managers and ApiManageHandler

--- e2xauthoring/app/handlers/apihandlers.py
+++ e2xauthoring/app/handlers/apihandlers.py
@@ -8,44 +8,17 @@ from nbgrader.server_extensions.formgrader.base import check_xsrf
 from tornado import web
 
 from ...converters import GenerateExercise
-from ...models import (
-    AssignmentModel,
-    ExerciseModel,
-    PresetModel,
-    TaskModel,
-    TaskPoolModel,
-    TemplateModel,
+from ...managers import (
+    AssignmentManager,
+    PresetManager,
+    TaskManager,
+    TaskPoolManager,
+    TemplateManager,
+    WorksheetManager,
 )
 from ...utils import NotebookVariableExtractor
 from ...utils.gitutils import get_author, set_author
-from .base import BaseApiListHandler, BaseApiManageHandler
-
-
-class PresetHandler(E2xApiHandler):
-    def initialize(self):
-        self.__model = PresetModel(self.coursedir)
-
-    def _list_template(self):
-        self.write(json.dumps(self.__model.list_template_presets()))
-
-    def _get_template(self):
-        name = self.get_argument("name")
-        self.write(json.dumps(self.__model.get_template_preset(name)))
-
-    def _list_question(self):
-        self.write(json.dumps(self.__model.list_question_presets()))
-
-    def _get_question(self):
-        name = self.get_argument("name")
-        self.write(json.dumps(self.__model.get_question_preset(name)))
-
-    @web.authenticated
-    @check_xsrf
-    def get(self):
-        action = self.get_argument("action")
-        preset_type = self.get_argument("type")
-        handler = getattr(self, "_{}_{}".format(action, preset_type))
-        handler()
+from .base import ApiManageHandler
 
 
 class TemplateVariableHandler(E2xApiHandler):
@@ -66,144 +39,6 @@ class KernelSpecHandler(E2xApiHandler):
     @check_xsrf
     def get(self):
         self.write(json.dumps(KernelSpecManager().get_all_specs()))
-
-
-class ManagePoolsHandler(E2xApiHandler):
-    def initialize(self) -> None:
-        self.__poolmodel = TaskPoolModel(self.coursedir)
-
-    @web.authenticated
-    @check_xsrf
-    def get(self, action):
-        if action == "list":
-            self.write(json.dumps(self.__poolmodel.list()))
-        elif action == "get":
-            self.write(json.dumps(self.__poolmodel.get(pool=self.get_argument("pool"))))
-        else:
-            self.write(
-                json.dumps(
-                    dict(status="error", message=f"{action} is not a valid action")
-                )
-            )
-
-    @web.authenticated
-    @check_xsrf
-    def post(self, action):
-        data = self.get_json_body()
-        name = data.get("name")
-        if action == "new":
-            create_repository = data.get("create_repository", False)
-            self.write(
-                json.dumps(
-                    self.__poolmodel.new(name=name, create_repository=create_repository)
-                )
-            )
-        elif action == "remove":
-            self.write(json.dumps(self.__poolmodel.remove(name=name)))
-        elif action == "init_repo":
-            self.write(json.dumps(self.__poolmodel.turn_into_repository(name)))
-        else:
-            self.write(
-                json.dumps(
-                    dict(status="error", message=f"{action} is not a valid action")
-                )
-            )
-
-
-class ManageTasksHandler(E2xApiHandler):
-    def initialize(self) -> None:
-        self.__model = TaskModel(self.coursedir)
-
-    @web.authenticated
-    @check_xsrf
-    def get(self, action):
-        if action == "list":
-            self.write(json.dumps(self.__model.list(pool=self.get_argument("pool"))))
-        elif action == "list_all":
-            tasks = []
-            pool_model = TaskPoolModel(self.coursedir)
-            for pool in pool_model.list():
-                tasks.extend(self.__model.list(pool["name"]))
-            self.write(json.dumps(tasks))
-        elif action == "get":
-            self.write(
-                json.dumps(
-                    self.__model.get(
-                        pool=self.get_argument("pool"), name=self.get_argument("name")
-                    )
-                )
-            )
-        elif action == "diff":
-            self.write(
-                json.dumps(
-                    self.__model.git_diff(
-                        pool=self.get_argument("pool"),
-                        task=self.get_argument("task"),
-                        file=self.get_argument("file"),
-                    )
-                )
-            )
-        else:
-            self.write(
-                json.dumps(
-                    dict(status="error", message=f"{action} is not a valid action")
-                )
-            )
-
-    @web.authenticated
-    @check_xsrf
-    def post(self, action):
-        data = self.get_json_body()
-        name = data.get("name")
-        pool = data.get("pool")
-        if action == "new":
-            self.write(json.dumps(self.__model.new(pool=pool, name=name)))
-        elif action == "remove":
-            self.write(json.dumps(self.__model.remove(pool=pool, name=name)))
-        elif action == "commit":
-            message = data.get("message", None)
-            self.write(
-                json.dumps(self.__model.commit(pool=pool, task=name, message=message))
-            )
-        else:
-            self.write(
-                json.dumps(
-                    dict(status="error", message=f"{action} is not a valid action")
-                )
-            )
-
-
-class ManageTemplatesHandler(E2xApiHandler):
-    def initialize(self) -> None:
-        self.__model = TemplateModel(self.coursedir)
-
-    @web.authenticated
-    @check_xsrf
-    def get(self, action):
-        if action == "list":
-            self.write(json.dumps(self.__model.list()))
-        else:
-            self.write(
-                json.dumps(
-                    dict(status="error", message=f"{action} is not a valid action")
-                )
-            )
-
-    @web.authenticated
-    @check_xsrf
-    def post(self, action):
-        data = self.get_json_body()
-        name = data.get("name")
-        if action == "new":
-            self.write(json.dumps(self.__model.new(name=name)))
-        elif action == "remove":
-            self.write(json.dumps(self.__model.remove(name=name)))
-        else:
-            self.write(
-                json.dumps(
-                    dict(status="error", message=f"{action} is not a valid action")
-                )
-            )
 
 
 class GenerateExerciseHandler(E2xApiHandler):
@@ -232,39 +67,86 @@ class GitAuthorHandler(E2xApiHandler):
 api_url = urljoin("e2x", "authoring", "api")
 name_regex = r"(?P<name>[^/]+)"
 assignment_regex = r"(?P<assignment>[^/]+)"
-pool_action_regex = r"(?P<action>new|list|remove|get|init_repo)"
-task_action_regex = r"(?P<action>new|list|list_all|remove|commit|get|diff)"
-template_action_regex = r"(?P<action>new|list|remove)"
 default_handlers = [
-    (urljoin(api_url, "presets"), PresetHandler),
     (
         urljoin(api_url, "assignments", "?"),
-        BaseApiListHandler,
-        dict(model_cls=AssignmentModel),
+        ApiManageHandler,
+        dict(
+            manager_cls=AssignmentManager,
+            actions=dict(
+                get=dict(actions=["list"]),
+            ),
+        ),
     ),
     (
-        urljoin(api_url, "template", name_regex, "?"),
-        BaseApiManageHandler,
-        dict(model_cls=TemplateModel),
+        urljoin(api_url, "worksheets", "?"),
+        ApiManageHandler,
+        dict(
+            manager_cls=WorksheetManager,
+            actions=dict(
+                get=dict(actions=["get", "list"]),
+                delete=dict(default="remove", actions=["remove"]),
+            ),
+        ),
+    ),
+    (
+        urljoin(api_url, "tasks", "?"),
+        ApiManageHandler,
+        dict(
+            manager_cls=TaskManager,
+            actions=dict(
+                delete=dict(default="remove", actions=["remove"]),
+                get=dict(
+                    default="get", actions=["get", "list", "list_all", "git_diff"]
+                ),
+                post=dict(default="create", actions=["create"]),
+                put=dict(actions=["copy", "rename", "commit"]),
+            ),
+        ),
     ),
     (
         urljoin(api_url, "templates", "?"),
-        BaseApiListHandler,
-        dict(model_cls=TemplateModel),
+        ApiManageHandler,
+        dict(
+            manager_cls=TemplateManager,
+            actions=dict(
+                delete=dict(default="remove", actions=["remove"]),
+                get=dict(default="get", actions=["get", "list"]),
+                post=dict(default="create", actions=["create"]),
+                put=dict(actions=["copy", "rename"]),
+            ),
+        ),
     ),
     (
-        urljoin(api_url, "worksheets", assignment_regex, name_regex, "?"),
-        BaseApiManageHandler,
-        dict(model_cls=ExerciseModel),
+        urljoin(api_url, "pools", "?"),
+        ApiManageHandler,
+        dict(
+            manager_cls=TaskPoolManager,
+            actions=dict(
+                delete=dict(default="remove", actions=["remove"]),
+                get=dict(default="get", actions=["get", "list"]),
+                post=dict(default="create", actions=["create"]),
+                put=dict(actions=["copy", "rename", "turn_into_repository"]),
+            ),
+        ),
     ),
     (
-        urljoin(api_url, "worksheets", assignment_regex, "?"),
-        BaseApiListHandler,
-        dict(model_cls=ExerciseModel),
+        urljoin(api_url, "presets", "?"),
+        ApiManageHandler,
+        dict(
+            manager_cls=PresetManager,
+            actions=dict(
+                get=dict(
+                    actions=[
+                        "list_question_presets",
+                        "list_template_presets",
+                        "get_question_preset",
+                        "get_template_preset",
+                    ]
+                )
+            ),
+        ),
     ),
-    (urljoin(api_url, "pools", pool_action_regex, "?"), ManagePoolsHandler),
-    (urljoin(api_url, "tasks", task_action_regex, "?"), ManageTasksHandler),
-    (urljoin(api_url, "templates", template_action_regex, "?"), ManageTemplatesHandler),
     (urljoin(api_url, "templates", "variables"), TemplateVariableHandler),
     (urljoin(api_url, "kernelspec"), KernelSpecHandler),
     (urljoin(api_url, "generate_worksheet"), GenerateExerciseHandler),
