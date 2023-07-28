052f7361295d16a1525fd55d1adc61fd1ec97a0b
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-07-28 18:02:37 +0200

    Remove status message from TaskManager

--- e2xauthoring/models/managers/taskmanager.py
+++ e2xauthoring/models/managers/taskmanager.py
@@ -6,8 +6,10 @@ from e2xcore.utils.nbgrader_cells import is_nbgrader_cell, new_read_only_cell
 from nbformat.v4 import new_notebook
 from traitlets import Unicode
 
+from e2xauthoring.models.managers.taskpoolmanager import TaskPoolManager
+
 from ...utils.gitutils import commit_path, vcs_status
-from ..dataclasses import ErrorMessage, SuccessMessage, Task
+from ..dataclasses import Task
 from .manager import BaseManager
 
 
@@ -36,19 +38,16 @@ class TaskManager(BaseManager):
         return points, questions
 
     def commit(self, pool, task, message):
-        path = os.path.join(self.base_path(), pool, task)
+        path = os.path.join(self.base_path, pool, task)
         git_status = self.git_status(pool, task)
-        if git_status["repo"] is None:
-            return dict(success=False, error="Not part of a git repository")
-        elif git_status["status"] == "unchanged":
-            return dict(
-                success=True, message="No files have been changed. Nothing to commit"
-            )
+        assert git_status is not None, f"Path {path} is not part of a git repository."
+        if git_status["status"] == "unchanged":
+            return "Nothing to commit. No files have been changed."
 
         commit_okay = commit_path(
             git_status["repo"], path, add_if_untracked=True, message=message
         )
-        return dict(success=commit_okay)
+        assert commit_okay, "There was an error during the commit process."
 
     def git_status(self, pool, task):
         path = os.path.join(self.base_path, pool, task)
@@ -64,44 +63,39 @@ class TaskManager(BaseManager):
     def git_diff(self, pool, task, file):
         path = os.path.join(self.base_path, pool, task, file)
         git_status = vcs_status(path)
-        if git_status["repo"] is None:
-            return dict(path=path, diff="Not version controlled or not added")
-        else:
-            relpath = os.path.relpath(path, start=git_status["repo"].working_tree_dir)
-            return dict(
-                path=path,
-                diff=git_status["repo"]
-                .git.diff(relpath, color=True)
-                .replace("\n", "<br/>"),
-            )
+        assert (
+            git_status["repo"] is not None
+        ), f"Path {path} is not version controlled or not added"
+        relpath = os.path.relpath(path, start=git_status["repo"].working_tree_dir)
+        return dict(
+            path=path,
+            diff=git_status["repo"]
+            .git.diff(relpath, color=True)
+            .replace("\n", "<br/>"),
+        )
 
-    def get(self, pool: str, name: str) -> SuccessMessage | ErrorMessage:
+    def get(self, pool: str, name: str):
         path = os.path.join(self.base_path, pool, name)
-        if not os.path.exists(path):
-            return ErrorMessage(error="The task does not exists")
+        assert os.path.exists(path), "The task does not exists"
         points, n_questions = self.__get_task_info(name, pool)
         git_status = self.git_status(pool, name)
         if "repo" in git_status:
             del git_status["repo"]
-        return SuccessMessage(
-            data=Task(
-                name=name,
-                pool=pool,
-                points=points,
-                n_questions=n_questions,
-                git_status=git_status,
-            )
+        return Task(
+            name=name,
+            pool=pool,
+            points=points,
+            n_questions=n_questions,
+            git_status=git_status,
         )
 
-    def create(self, pool: str, name: str) -> SuccessMessage | ErrorMessage:
-        if not self.is_valid_name(name):
-            return ErrorMessage(error="The name is invalid!")
+    def create(self, pool: str, name: str):
+        assert self.is_valid_name(name), "The name is invalid!"
         path = os.path.join(self.base_path, pool, name)
-        if os.path.exists(path):
-            return ErrorMessage(
-                error=f"A task with the name {name} already exists in the pool {pool}!"
-            )
-        self.log.info(f"Creating new template with name {name}")
+        assert not os.path.exists(
+            path
+        ), f"A task with the name {name} already exists in the pool {pool}!"
+        self.log.info(f"Creating new task with name {name}")
         os.makedirs(os.path.join(path, "img"), exist_ok=True)
         os.makedirs(os.path.join(path, "data"), exist_ok=True)
         nb = new_notebook(metadata=dict(nbassignment=dict(type="task")))
@@ -116,24 +110,23 @@ class TaskManager(BaseManager):
         )
         nb.cells.append(cell)
         nbformat.write(nb, os.path.join(path, f"{name}.ipynb"))
-        return SuccessMessage()
 
-    def remove(self, pool, name) -> SuccessMessage | ErrorMessage:
+    def remove(self, pool, name):
         path = os.path.join(self.base_path, pool, name)
-        if not os.path.exists(path):
-            return ErrorMessage(error="The task does not exist")
+        assert os.path.exists(
+            path
+        ), f"No task with the name {name} from pool {pool} exists."
         shutil.rmtree(path)
-        return SuccessMessage()
 
-    def list(self, pool) -> SuccessMessage | ErrorMessage:
+    def list(self, pool):
         tasks = []
         path = os.path.join(self.base_path, pool)
-        if not os.path.exists(path):
-            return ErrorMessage(error=f"No pool with the name {pool} exists.")
+        assert os.path.exists(path), f"No pool with the name {pool} exists."
         for task_dir in self.listdir(os.path.join(self.base_path, pool)):
             points, n_questions = self.__get_task_info(task_dir, pool)
             git_status = self.git_status(pool, task_dir)
-            del git_status["repo"]
+            if "repo" in git_status:
+                del git_status["repo"]
             tasks.append(
                 Task(
                     name=task_dir,
@@ -143,16 +136,19 @@ class TaskManager(BaseManager):
                     git_status=git_status,
                 )
             )
-        return SuccessMessage(data=tasks)
+        return tasks
 
-    def copy(
-        self, old_name: str, new_name: str, pool: str = ""
-    ) -> ErrorMessage | SuccessMessage:
+    def list_all(self):
+        pool_manager = TaskPoolManager(self.coursedir)
+        tasks = []
+        for pool in pool_manager.list():
+            tasks.extend(self.list(pool.name))
+        return tasks
+
+    def copy(self, old_name: str, new_name: str, pool: str = ""):
         src = os.path.join(pool, old_name)
         dst = os.path.join(pool, new_name)
-        status = super().copy(src, dst)
-        if not status.success:
-            return status
+        super().copy(src, dst)
         dst_path = os.path.join(self.base_path, dst)
         shutil.move(
             os.path.join(dst_path, f"{old_name}.ipynb"),
@@ -166,12 +162,6 @@ class TaskManager(BaseManager):
                 cell.metadata.nbgrader.grade_id = grade_id.replace(old_name, new_name)
         nbformat.write(nb, nb_path)
 
-        return SuccessMessage()
-
-    def rename(
-        self, old_name: str, new_name: str, pool: str = ""
-    ) -> ErrorMessage | SuccessMessage:
-        msg = self.copy(old_name, new_name, pool)
-        if not msg.success:
-            return msg
+    def rename(self, old_name: str, new_name: str, pool: str = ""):
+        self.copy(old_name, new_name, pool)
         self.remove(pool, old_name)
