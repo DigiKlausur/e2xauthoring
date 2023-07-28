9bb16e8baf017bcc0ed6716ea0cdaeae131833c8
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-07-28 18:02:54 +0200

    Update managers

--- e2xauthoring/models/managers/__init__.py
+++ e2xauthoring/models/managers/__init__.py
@@ -1,6 +1,13 @@
 from .manager import BaseManager
+from .presetmanager import PresetManager
 from .taskmanager import TaskManager
 from .taskpoolmanager import TaskPoolManager
 from .templatemanager import TemplateManager
 
-__all__ = ["BaseManager", "TaskManager", "TaskPoolManager", "TemplateManager"]
+__all__ = [
+    "BaseManager",
+    "PresetManager",
+    "TaskManager",
+    "TaskPoolManager",
+    "TemplateManager",
+]
