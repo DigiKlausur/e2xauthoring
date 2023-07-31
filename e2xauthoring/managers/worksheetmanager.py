e2e1e5bb8338bcf5c319d737aaa407737bb1adca
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-07-31 17:30:53 +0200

    Add create method to WorksheetManager

--- e2xauthoring/managers/worksheetmanager.py
+++ e2xauthoring/managers/worksheetmanager.py
@@ -2,16 +2,26 @@ import glob
 import os
 import shutil
 
+from nbgrader.coursedir import CourseDirectory
 from traitlets import Unicode
 
+from ..converters import GenerateExercise
 from .base import BaseManager
 
 
 class WorksheetManager(BaseManager):
     directory = Unicode("source", help="The directory where assignments go.")
 
+    def __init__(self, coursedir: CourseDirectory) -> None:
+        super().__init__(coursedir)
+        self.converter = GenerateExercise(coursedir=coursedir)
+
     def get(self, name: str, assignment: str):
         return {"name": name, "assignment": assignment}
+
+    def create(self, resources):
+        self.log.info(resources)
+        self.converter.convert(resources)
 
     def remove(self, name: str, assignment: str):
         base_path = os.path.join(self.base_path, assignment)
