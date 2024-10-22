6a4344631fc73694975424b67cc9bfbaf5b114da
Tim Daniel Metzler <tim.metzler@h-brs.de>
2024-10-22 17:13:43 +0200

    Add kernel name to create task dialog

--- packages/app/src/components/task/dialogs/NewTaskDialog.jsx
+++ packages/app/src/components/task/dialogs/NewTaskDialog.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
-import { Stack } from "@mui/material";
+import {
+  Stack,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 
 import API from "@e2xauthoring/api";
 import { FormDialogWithButton } from "../../dialogs/form-dialogs";
@@ -11,13 +17,16 @@ import { getTaskUrl } from "../../../utils/urls";
 
 export default function NewTaskDialog({ pool }) {
   const navigate = useNavigate();
+  const [kernels, setKernels] = React.useState({});
+
   const formik = useFormik({
     initialValues: {
       name: "",
+      kernel: "",
     },
     validationSchema: baseSchema,
     onSubmit: (values) => {
-      API.tasks.create(pool, values.name).then((status) => {
+      API.tasks.create(pool, values.name, values.kernel).then((status) => {
         if (!status.success) {
           alert(status.error);
         } else {
@@ -26,6 +35,18 @@ export default function NewTaskDialog({ pool }) {
       });
     },
   });
+
+  React.useEffect(() => {
+    API.kernels.list().then((_kernels) => {
+      setKernels(_kernels);
+      if (
+        Object.keys(_kernels).length > 0 &&
+        formik.values.kernel.length === 0
+      ) {
+        formik.setFieldValue("kernel", Object.keys(_kernels)[0]);
+      }
+    });
+  }, [formik]);
 
   return (
     <FormDialogWithButton
@@ -37,6 +58,22 @@ export default function NewTaskDialog({ pool }) {
       <form onSubmit={formik.handleSubmit}>
         <Stack spacing={2} sx={{ width: "30ch" }}>
           <FormikTextField formik={formik} name="name" label="Task Name" />
+          <FormControl>
+            <InputLabel id="kernel-label">Kernel</InputLabel>
+            <Select
+              labelId="kernel-label"
+              label="Kernel"
+              value={formik.values.kernel}
+              onChange={formik.handleChange}
+              name="kernel"
+            >
+              {Object.keys(kernels).map((kernel) => (
+                <MenuItem key={kernel} value={kernel}>
+                  {kernels[kernel].spec.display_name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Stack>
       </form>
     </FormDialogWithButton>
