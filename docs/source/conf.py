bc0b6525a3ed19cdb676bbc7b79eae81604d033a
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-02-20 14:42:19 +0100

    Bump to 0.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "e2xauthoring"
 copyright = "2023, Tim Metzler"
 author = "Tim Metzler"
-release = "0.3.0"
+release = "0.3.1"
 github_project_url = "https://github.com/Digiklausur/e2xauthoring"
 
 # -- General configuration ---------------------------------------------------
